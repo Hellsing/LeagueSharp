04032f9ce1c20bfeea63c9fdb8fa53746f75c4a3
Hellsing <matrix11@gmx.net>
2014-08-18 22:42:33 +0200

    Added harass and waveclear, improved stuff

--- BrandSharp/Program.cs
+++ BrandSharp/Program.cs
@@ -10,6 +10,8 @@ using LeagueSharp.Common;
 
 using SharpDX;
 
+using Color = System.Drawing.Color;
+
 namespace BrandSharp
 {
     class Program
@@ -20,6 +22,9 @@ namespace BrandSharp
         private static Spell Q, W, E, R;
         private static readonly List<Spell> spellList = new List<Spell>();
         private static readonly int bounceRadiusR = 450;
+
+        private static bool hasIgnite = false;
+        private static SpellSlot igniteSlot;
 
         private static Orbwalking.Orbwalker OW;
         private static Menu menu;
@@ -53,12 +58,21 @@ namespace BrandSharp
             // Add to spell list
             spellList.AddRange(new[] { Q, W, E, R });
 
+            // Ignite check
+            var ignite = player.Spellbook.GetSpell(player.GetSpellSlot("SummonerDot"));
+            if (ignite != null && ignite.Slot != SpellSlot.Unknown)
+            {
+                hasIgnite = true;
+                igniteSlot = ignite.Slot;
+            }
+
             // Define main combo
             mainCombo.Add(Tuple.Create(DamageLib.SpellType.AD, DamageLib.StageType.Default));
             mainCombo.Add(Tuple.Create(DamageLib.SpellType.Q, DamageLib.StageType.Default));
             mainCombo.Add(Tuple.Create(DamageLib.SpellType.W, DamageLib.StageType.Default));
             mainCombo.Add(Tuple.Create(DamageLib.SpellType.E, DamageLib.StageType.Default));
             mainCombo.Add(Tuple.Create(DamageLib.SpellType.R, DamageLib.StageType.FirstDamage));
+            mainCombo.Add(Tuple.Create(DamageLib.SpellType.IGNITE, DamageLib.StageType.Default));
 
             // Define bounce combo
             bounceCombo.Add(Tuple.Create(DamageLib.SpellType.R, DamageLib.StageType.FirstDamage));
@@ -69,6 +83,7 @@ namespace BrandSharp
 
             // Register events
             Game.OnGameUpdate += Game_OnGameUpdate;
+            Drawing.OnDraw += Drawing_OnDraw;
         }
 
         static void Game_OnGameUpdate(EventArgs args)
@@ -101,6 +116,9 @@ namespace BrandSharp
             bool useE = menu.SubMenu("combo").Item("comboUseE").GetValue<bool>();
             bool useR = menu.SubMenu("combo").Item("comboUseR").GetValue<bool>();
 
+            if (IsKillable(target, mainCombo))
+                CastIgnite(target);
+
             foreach (var spell in spellList)
             {
                 // Continue if spell not ready
@@ -127,7 +145,7 @@ namespace BrandSharp
                         (IsAblazed(target)) || // Ablazed
                         (IsKillable(player, new[] { DamageLib.SpellType.W })) || // Killable
                         (Vector2.DistanceSquared(target.ServerPosition.To2D(), player.Position.To2D()) > E.Range * E.Range) ||
-                        (!E.IsReady() && player.Spellbook.GetSpell(SpellSlot.E).CooldownExpires > player.Spellbook.GetSpell(SpellSlot.W).Cooldown)) // Cooldown substraction E ready
+                        (!E.IsReady() && player.Spellbook.GetSpell(SpellSlot.E).CooldownExpires - Game.Time > player.Spellbook.GetSpell(SpellSlot.W).Cooldown)) // Cooldown substraction E ready
                     {
                         // Cast W on high hitchance
                         W.CastIfHitchanceEquals(target, Prediction.HitChance.HighHitchance);
@@ -173,12 +191,136 @@ namespace BrandSharp
 
         private static void OnHarass()
         {
-            ;
+            // Target aquireing
+            var target = SimpleTs.GetTarget(W.Range, SimpleTs.DamageType.Magical);
+
+            // Target validation
+            if (target == null)
+                return;
+
+            // Spell usage
+            bool useQ = menu.SubMenu("harass").Item("harassUseQ").GetValue<bool>();
+            bool useW = menu.SubMenu("harass").Item("harassUseW").GetValue<bool>() || menu.SubMenu("harass").Item("harassToggleW").GetValue<bool>();
+            bool useE = menu.SubMenu("harass").Item("harassUseE").GetValue<bool>();
+
+            foreach (var spell in spellList)
+            {
+                // Continue if spell not ready
+                if (!spell.IsReady())
+                    continue;
+
+                // Q
+                if (spell.Slot == SpellSlot.Q && useQ)
+                {
+                    if ((IsAblazed(target)) || // Ablazed
+                        (IsKillable(player, new[] { DamageLib.SpellType.Q })) || // Killable
+                        (!useW && !useE) || // Casting when not using W and E
+                        (useW && !useE && !W.IsReady() && player.Spellbook.GetSpell(SpellSlot.W).CooldownExpires - Game.Time > player.Spellbook.GetSpell(SpellSlot.Q).Cooldown) || // Cooldown substraction W ready, jodus please...
+                        ((useE && !useW || useW && useE) && !E.IsReady() && player.Spellbook.GetSpell(SpellSlot.E).CooldownExpires - Game.Time > player.Spellbook.GetSpell(SpellSlot.Q).Cooldown)) // Cooldown substraction E ready, jodus please...
+                    {
+                        // Cast Q on high hitchance
+                        Q.CastIfHitchanceEquals(target, Prediction.HitChance.HighHitchance);
+                    }
+                }
+                // W
+                else if (spell.Slot == SpellSlot.W && useW)
+                {
+                    if ((menu.SubMenu("harass").Item("harassToggleW").GetValue<bool>()) || // Harass toggle
+                        (!useE) || // Casting when not using E
+                        (IsAblazed(target)) || // Ablazed
+                        (IsKillable(player, new[] { DamageLib.SpellType.W })) || // Killable
+                        (Vector2.DistanceSquared(target.ServerPosition.To2D(), player.Position.To2D()) > E.Range * E.Range) ||
+                        (!E.IsReady() && player.Spellbook.GetSpell(SpellSlot.E).CooldownExpires - Game.Time > player.Spellbook.GetSpell(SpellSlot.W).Cooldown)) // Cooldown substraction E ready
+                    {
+                        // Cast W on high hitchance
+                        W.CastIfHitchanceEquals(target, Prediction.HitChance.HighHitchance);
+                    }
+                }
+                // E
+                else if (spell.Slot == SpellSlot.E && useE)
+                {
+                    // Distance check
+                    if (Vector2.DistanceSquared(target.ServerPosition.To2D(), player.Position.To2D()) < E.Range * E.Range)
+                    {
+                        if ((!useQ && !useW) || // Casting when not using Q and W
+                            IsKillable(player, new[] { DamageLib.SpellType.E }) || // Killable
+                            (useQ && (Q.IsReady() || player.Spellbook.GetSpell(SpellSlot.Q).Cooldown < 5)) || // Q ready
+                            (useW && W.IsReady())) // W ready
+                        {
+                            // Cast E on target
+                            E.CastOnUnit(target);
+                        }
+                    }
+                }
+            }
         }
 
         private static void OnWaveClear()
         {
-            ;
+            // Minions around
+            var minions = MinionManager.GetMinions(player.Position, W.Range + W.Width / 2);
+
+            // Spell usage
+            bool useQ = Q.IsReady() && menu.SubMenu("waveClear").Item("waveUseQ").GetValue<bool>();
+            bool useW = W.IsReady() && menu.SubMenu("waveClear").Item("waveUseW").GetValue<bool>();
+            bool useE = E.IsReady() && menu.SubMenu("waveClear").Item("waveUseE").GetValue<bool>();
+
+            if (useQ)
+            {
+                // Loop through all minions to find a target, preferred a killable one
+                Obj_AI_Base target = null;
+                foreach (var minion in minions)
+                {
+                    var prediction = Q.GetPrediction(minion);
+                    if (prediction.HitChance == Prediction.HitChance.HighHitchance)
+                    {
+                        // Set target
+                        target = minion;
+
+                        // Break if killlable
+                        if (minion.Health > DamageLib.getDmg(minion, DamageLib.SpellType.AD) && IsKillable(minion, new[] { DamageLib.SpellType.Q }))
+                            break;
+                    }
+                }
+
+                // Cast if target found
+                if (target != null)
+                    Q.Cast(target);
+            }
+
+            if (useW)
+            {
+                // Get farm location
+                var farmLocation = MinionManager.GetBestCircularFarmLocation(minions.Select(minion => minion.ServerPosition.To2D()).ToList(), W.Width, W.Range);
+
+                // Check required hitnumber and cast
+                if (farmLocation.MinionsHit >= menu.SubMenu("waveClear").Item("waveNumW").GetValue<Slider>().Value)
+                    W.Cast(farmLocation.Position);
+            }
+
+            if (useE)
+            {
+                // Loop through all minions to find a target
+                foreach (var minion in minions)
+                {
+                    // Distance check
+                    if (Vector2.DistanceSquared(minion.ServerPosition.To2D(), player.Position.To2D()) < E.Range * E.Range)
+                    {
+                        // E only on targets that are ablaze or killable
+                        if (IsAblazed(minion) || minion.Health > DamageLib.getDmg(minion, DamageLib.SpellType.AD) && IsKillable(minion, new[] { DamageLib.SpellType.E }))
+                        {
+                            E.CastOnUnit(minion);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void CastIgnite(Obj_AI_Hero target)
+        {
+            if (hasIgnite && player.Spellbook.GetSpell(player.GetSpellSlot("SummonerDot")).State == SpellState.Ready)
+                player.SummonerSpellbook.CastSpell(igniteSlot, target);
         }
 
         private static bool IsKillable(Obj_AI_Base target, IEnumerable<DamageLib.SpellType> spellCombo)
@@ -201,6 +343,11 @@ namespace BrandSharp
                         spellIncluded = true;
                     }
                 }
+                else if (spell.Item1 == DamageLib.SpellType.IGNITE)
+                {
+                    if (hasIgnite && player.Spellbook.GetSpell(player.GetSpellSlot("SummonerDot")).State == SpellState.Ready)
+                        damage += DamageLib.getDmg(target, DamageLib.SpellType.IGNITE);
+                }
             }
             return damage + (spellIncluded ? target.MaxHealth * 0.08 : 0) > target.Health;
         }
@@ -208,6 +355,17 @@ namespace BrandSharp
         private static bool IsAblazed(Obj_AI_Base target)
         {
             return target.HasBuff("brandablaze", true);
+        }
+
+        private static void Drawing_OnDraw(EventArgs args)
+        {
+            // Spell ranges
+            foreach (var spell in spellList)
+            {
+                var circleEntry = menu.SubMenu("drawings").Item("drawRange" + spell.Slot.ToString()).GetValue<Circle>();
+                if (circleEntry.Active)
+                    Utility.DrawCircle(player.Position, spell.Range, circleEntry.Color);
+            }
         }
 
         private static void SetupMenu()
@@ -239,6 +397,7 @@ namespace BrandSharp
             menu.AddSubMenu(harass);
             harass.AddItem(new MenuItem("harassUseQ", "Use Q").SetValue(true));
             harass.AddItem(new MenuItem("harassUseW", "Use W").SetValue(true));
+            harass.AddItem(new MenuItem("harassToggleW", "Use W (toggle)").SetValue<KeyBind>(new KeyBind('T', KeyBindType.Toggle)));
             harass.AddItem(new MenuItem("harassUseE", "Use E").SetValue(true));
             harass.AddItem(new MenuItem("harassActive", "Harass active").SetValue<KeyBind>(new KeyBind('C', KeyBindType.Press)));
 
@@ -250,6 +409,14 @@ namespace BrandSharp
             waveClear.AddItem(new MenuItem("waveUseE", "Use E").SetValue(true));
             waveClear.AddItem(new MenuItem("waveNumW", "Minions to hit with W").SetValue<Slider>(new Slider(3, 1, 10)));
             waveClear.AddItem(new MenuItem("waveActive", "WaveClear active").SetValue<KeyBind>(new KeyBind('V', KeyBindType.Press)));
+
+            // Drawings
+            Menu drawings = new Menu("Drawings", "drawings");
+            menu.AddSubMenu(drawings);
+            drawings.AddItem(new MenuItem("drawRangeQ", "Q range").SetValue(new Circle(true, Color.FromArgb(150, Color.IndianRed))));
+            drawings.AddItem(new MenuItem("drawRangeW", "W range").SetValue(new Circle(true, Color.FromArgb(150, Color.IndianRed))));
+            drawings.AddItem(new MenuItem("drawRangeE", "E range").SetValue(new Circle(false, Color.FromArgb(150, Color.DarkRed))));
+            drawings.AddItem(new MenuItem("drawRangeR", "R range").SetValue(new Circle(false, Color.FromArgb(150, Color.Red))));
 
             // Finalize menu
             menu.AddToMainMenu();
